783b357c3e3911ae3f8380dec53ee1abc1ccf9f4
Sameen-R <61333498+Sameen-R@users.noreply.github.com>
2020-05-24 12:54:37 -0500

    Update DrawMap.java

--- src/mapSetup/DrawMap.java
+++ src/mapSetup/DrawMap.java
@@ -11,15 +11,19 @@ public class DrawMap {
 	public DrawMap() {
 		bricks = new ArrayList<Brick>();
 		spikes = new ArrayList<Spike>();
+		for(int i=0; i<5; i++) {
+			addStuff( (int)(Math.random()*1000)+1000 );
+		}
 	}
 	public void drawAll(Graphics g, int x, int y) {
 		Graphics2D g2d = (Graphics2D) g;
+		
 		for (Brick brick: bricks) {
 			brick.drawImage(x, y, g2d);
 		}
 	}
-	public void addStuff() {
-		bricks.add(new Brick(600, 500, 0));
+	public void addStuff(int x) {
+		bricks.add(new Brick(x, 0));
 	}
 
 }
