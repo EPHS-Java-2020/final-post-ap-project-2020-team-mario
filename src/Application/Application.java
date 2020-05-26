de4b9eb1f201fa7fa3dadc668d9354210fb36cba
Sameen-R <61333498+Sameen-R@users.noreply.github.com>
2020-05-26 10:14:17 -0500

    StartingScreen changes

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -14,15 +14,25 @@ public class Application extends JFrame {
     }
 
     private void initUI() {
-
+    	StartScreen ss = new StartScreen();
+    	
+    	add(ss);
+    	setSize(2000, 1000);
+    	ss.waitFor();
+    	remove(ss);
+    	
+    	
+    	
         add(new Board());
 
-        setSize(2000, 1000);
+        //setSize(2000, 1000);
 
         setTitle("Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        
     }  
+     
     
     
     public static void main(String[] args) {
