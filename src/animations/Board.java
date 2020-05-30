0a4d405f7a71d1ac6fac546acafa5a6e0e236ef8
Sarthak Agrawal <sarthavader@gmail.com>
2020-05-30 10:11:42 -0500

    nothing

--- src/animations/Board.java
+++ src/animations/Board.java
@@ -135,7 +135,7 @@ public class Board extends JPanel implements Runnable {
 			Font retryTitle  = new Font("arial", Font.BOLD, 20);
 			g.setFont(retryTitle);
 			g.setColor(Color.white);
-			g.drawString("Retry", 1775, 65);
+			g.drawString("Retry", 775, 65);
 			g2d.draw(new Rectangle(1750, 35, 100, 50));
 			
 		}else {
