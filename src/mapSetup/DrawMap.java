165150e0b6ddc77d3d4fb2bde057892ddf507bde
Sameen-R <61333498+Sameen-R@users.noreply.github.com>
2020-05-29 20:25:41 -0500

    level2Map

--- src/mapSetup/DrawMap.java
+++ src/mapSetup/DrawMap.java
@@ -101,68 +101,83 @@ public class DrawMap {
 	}
 	public void addStuff() {
 		if (levels.level == 1) {
-			floors.add(new Floor(500 ,1100, 0, 4000));
-			bricks.add(new Brick(1400,1040, 0));
-			spikes.add(new Spike(1500,1050,0));
-			spikes.add(new Spike(1700,1050,0));
-			trees.add(new Tree(800,550,0));
-			suns.add(new Sun(200,400,0));
-			bricks.add(new Brick(1850,1040, 0));
-			bricks.add(new Brick(1850,975, 0));
-			cloudes.add(new Cloud(1200,400,0));
-			cloudes.add(new Cloud(1800,500,0));
-			bricks.add(new Brick(2150,1040, 0));
-			bricks.add(new Brick(2150,975, 0));
-			bricks.add(new Brick(2430,1040, 0));
-			bricks.add(new Brick(2430,975, 0));
-			spikes.add(new Spike(2200,1050,0));
-			spikes.add(new Spike(2290,1050,0));
-			spikes.add(new Spike(2380,1050,0));
-			chickens.add(new Chicken(1910,1000,0, 1, 180));
-
-			floors.add(new Floor(3200,925,0, 2500));
-			floors.add(new Floor(3200,1000,0, 2500));
-			chickens.add(new Chicken(2950,1000,0,2,100));
-			
-
-			bricks.add(new Brick(3400,865,0));
-			chickens.add(new Chicken(3460, 825, 0, 3, 26));
-			
-			
-			bricks.add(new Brick(3600,865,0));
-			bricks.add(new Brick(3600,800,0));
-		
-			spikes.add(new Spike(3650,875,0));
-			spikes.add(new Spike(3700,875,0));
-			
-			
-			bricks.add(new Brick(3750,865,0));
-			bricks.add(new Brick(3750,800,0));
-			bricks.add(new Brick(3750,735,0));
-			
-			bricks.add(new Brick(3825,865,0));
-			bricks.add(new Brick(3825,800,0));
-			bricks.add(new Brick(3825,735,0));
-			bricks.add(new Brick(3825,670,0));
-			bricks.add(new Brick(3825,605,0));
-			
-
-			spikes.add(new Spike(3875,875,0));
-			spikes.add(new Spike(3925,875,0));
-			spikes.add(new Spike(3975,875,0));
-			spikes.add(new Spike(4025,875,0));
-			spikes.add(new Spike(4075,875,0));
-			spikes.add(new Spike(4125,875,0));
-			
-			bricks.add(new Brick(4200,865,0));
-			bricks.add(new Brick(4200,800,0));
-			
-			cars.add(new Car(4700, 700, 0));
-			
+			setUpLevel1();
+		}else if(levels.level==2) {
+			setUpLevel2();
 		}
 
 	}
 	
+	public void setUpLevel2() {
+		floors.add(new Floor(1000 ,1300, 0, 4000));
+		floors.add(new Floor(1400 ,900, 0, 4000));
+		bricks.add(new Brick(1200,1240, 0));
+		bricks.add(new Brick(1200,1175, 0));
+		bricks.add(new Brick(1250,1110, 0));
+		bricks.add(new Brick(1250,1045, 0));
+		enemies.add(new Enemy(2700, 675, 3, 400));
+	}
+	
+	public void setUpLevel1()
+	{
+		floors.add(new Floor(500 ,1100, 0, 4000));
+		bricks.add(new Brick(1400,1040, 0));
+		spikes.add(new Spike(1500,1050,0));
+		spikes.add(new Spike(1700,1050,0));
+		trees.add(new Tree(800,550,0));
+		suns.add(new Sun(200,400,0));
+		bricks.add(new Brick(1850,1040, 0));
+		bricks.add(new Brick(1850,975, 0));
+		cloudes.add(new Cloud(1200,400,0));
+		cloudes.add(new Cloud(1800,500,0));
+		bricks.add(new Brick(2150,1040, 0));
+		bricks.add(new Brick(2150,975, 0));
+		bricks.add(new Brick(2430,1040, 0));
+		bricks.add(new Brick(2430,975, 0));
+		spikes.add(new Spike(2200,1050,0));
+		spikes.add(new Spike(2290,1050,0));
+		spikes.add(new Spike(2380,1050,0));
+		chickens.add(new Chicken(1910,1000,0, 1, 180));
+
+		floors.add(new Floor(3200,925,0, 2500));
+		floors.add(new Floor(3200,1000,0, 2500));
+		chickens.add(new Chicken(2950,1000,0,2,100));
+		
+
+		bricks.add(new Brick(3400,865,0));
+		chickens.add(new Chicken(3460, 825, 0, 3, 26));
+		
+		
+		bricks.add(new Brick(3600,865,0));
+		bricks.add(new Brick(3600,800,0));
+	
+		spikes.add(new Spike(3650,875,0));
+		spikes.add(new Spike(3700,875,0));
+		
+		
+		bricks.add(new Brick(3750,865,0));
+		bricks.add(new Brick(3750,800,0));
+		bricks.add(new Brick(3750,735,0));
+		
+		bricks.add(new Brick(3825,865,0));
+		bricks.add(new Brick(3825,800,0));
+		bricks.add(new Brick(3825,735,0));
+		bricks.add(new Brick(3825,670,0));
+		bricks.add(new Brick(3825,605,0));
+		
+
+		spikes.add(new Spike(3875,875,0));
+		spikes.add(new Spike(3925,875,0));
+		spikes.add(new Spike(3975,875,0));
+		spikes.add(new Spike(4025,875,0));
+		spikes.add(new Spike(4075,875,0));
+		spikes.add(new Spike(4125,875,0));
+		
+		bricks.add(new Brick(4200,865,0));
+		bricks.add(new Brick(4200,800,0));
+		
+		cars.add(new Car(4700, 720, 0));
+	}
 	public ArrayList<Chicken> getChickens() {
 		return chickens;
 	}
